96213ecbee10b37d5bcaa74ff85fe6ff4b54471c
Mark Carman <carman.md@live.com>
2024-02-10 03:58:57 -0700

    remove datafiles from tautulli and syncthing

--- apps/airsonic/config/rollback.sql
+++ apps/airsonic/config/rollback.sql
@@ -2,13 +2,13 @@
 -- SQL to roll back currently unexecuted changes
 -- *********************************************************************
 -- Change Log: classpath:liquibase/db-changelog.xml
--- Ran at: 2/8/24, 5:46 PM
+-- Ran at: 2/10/24, 3:13 AM
 -- Against: SA@jdbc:hsqldb:file:/config/db/airsonic;hsqldb.tx=mvcc;sql.enforce_size=false;sql.char_literal=false;sql.nulls_first=false;sql.pad_space=false;hsqldb.defrag_limit=50;shutdown=true
 -- Liquibase version: 4.6.2
 -- *********************************************************************
 
 -- Lock Database
-UPDATE PUBLIC.DATABASECHANGELOGLOCK SET LOCKED = TRUE, LOCKEDBY = '67e889082322 (10.5.0.4)', LOCKGRANTED = '2024-02-08 17:46:39.706' WHERE ID = 1 AND LOCKED = FALSE;
+UPDATE PUBLIC.DATABASECHANGELOGLOCK SET LOCKED = TRUE, LOCKEDBY = '67e889082322 (10.5.0.5)', LOCKGRANTED = '2024-02-10 03:13:49.311' WHERE ID = 1 AND LOCKED = FALSE;
 
 -- Release Database Lock
 UPDATE PUBLIC.DATABASECHANGELOGLOCK SET LOCKED = FALSE, LOCKEDBY = NULL, LOCKGRANTED = NULL WHERE ID = 1;
